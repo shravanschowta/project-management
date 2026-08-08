0272f45694a6fb5b922b708901e30fb5baaf61f8
Shravan <shravanschowta@gmail.com>
2026-08-08 10:31:35 +0530

    first commit

---
+++ server/configs/prisma.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { PrismaClient } from './generated/prisma';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import {neonConfig} from '@neondatabse/serverless';
+
+import ws from 'ws';
+neonConfig.webSocketConstructor=ws;
+
+const connectionString=`${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({
+  connectionString
+})
+
+const prisma=global.prisma || new PrismaClient({adapter});
+
+if(process.env.NODE_ENV==='development') global.prisma=prisma;
+
+
+export default prisma;
---
+++ server/inngest/index.js
@@ -0,0 +1,61 @@
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "project-management" });
+
+//ingest function to create user data in database
+const syncUserCreation=inngest.createFunction(
+    {id:'sync-user-from-clerk'},
+    {event:'clerk/user.created'},
+    async({event})=>{
+        const{data}=event;
+        await prisma.user.create({
+            data:{
+                id:data.id,
+                email:data?.email_addresses[0]?.email_addres,
+                name:data?.first_name+" "+data?.last_name,
+                image:data?.image_url,
+            }
+        })
+    }
+)
+
+//user funtion to delete user data from database
+const syncUserDeletion=inngest.createFunction(
+    {id:'delete-user-with-clerk'},
+    {event:'clerk/user.deleted'},
+    async({event})=>{
+        const{data}=event;
+        await prisma.user.delete({
+            where:{
+                id:data.id,
+            }
+        })
+    }
+)
+
+//update
+const syncUserUpdation=inngest.createFunction(
+    {id:'update-user-from-clerk'},
+    {event:'clerk/user.updated'},
+    async({event})=>{
+        const{data}=event;
+        await prisma.user.update({
+            where:{
+                id:data.id,
+            },
+            data:{
+                email:data?.email_addresses[0]?.email_addres,
+                name:data?.first_name+" "+data?.last_name,
+                image:data?.image_url,
+            }
+        })
+    }
+)
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation
+];

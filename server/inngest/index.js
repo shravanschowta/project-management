cfdf2f734c43be318da522d34b0a1d6ee53f1cbb
Shravan <shravanschowta@gmail.com>
2026-08-08 22:52:04 +0530

    fixed user creation

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -12,7 +12,7 @@ const syncUserCreation=inngest.createFunction(
         await prisma.user.create({
             data:{
                 id:data.id,
-                email:data?.email_addresses[0]?.email_addres,
+                email:data?.email_addresses[0]?.email_address,
                 name:data?.first_name+" "+data?.last_name,
                 image:data?.image_url,
             }
@@ -43,7 +43,7 @@ const syncUserUpdation=inngest.createFunction(
                 id:data.id,
             },
             data:{
-                email:data?.email_addresses[0]?.email_addres,
+                email:data?.email_addresses[0]?.email_address,
                 name:data?.first_name+" "+data?.last_name,
                 image:data?.image_url,
             }
